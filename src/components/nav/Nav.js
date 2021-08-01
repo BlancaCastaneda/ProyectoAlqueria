fc77f5a458e4ad2c191a1fc17b1939a53cdacbdf
BlancaCastaneda <blankcf1809@gmail.com>
2021-08-01 09:34:30 -0500

    Se realiza ajustes a los archivos

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -6,29 +6,31 @@ import{BrowserRouter as Router,Link} from "react-router-dom";
 function Nav(){
     return (
         <div>
-            <nav>
+            <nav className="navbar">
+
                 <div className="logo">
                     <img src={logo} className="img-fluid"/>
                 </div>
 
-                <div>
-                    <ul type="none" className="menu">
+                <div className="menu">
+                    <ul className="nav" type="none">
+                        
                         <li className="item">
-                            <Link to="/">Home</Link>
+                            <Link to="/home"><i class="fas fa-home" ></i> Home</Link>
                         </li>
 
                         <li className="item">
-                            <Link to="/granjas">Granjas</Link>
+                            <Link to="/granjas"><i class="fas fa-tractor"></i>Granjas</Link>
                         </li>
 
                         <li className="item">
-                            <Link to="/produccionlactea">Producción Láctea</Link>
+                            <Link to="/produccionlactea"><i class="fas fa-glass-whiskey"></i>Producción Láctea</Link>
                         </li>
                     </ul>
                 </div>
 
                 <div className="boton">
-                    <button className="cerrarsesion"><i class="fas fa-sign-out-alt"></i>Cerrar Sesión</button>
+                    <button className="cerrarsesion"><i class="fas fa-sign-out-alt"></i> Cerrar Sesión</button>
                 </div>
             </nav>
         </div>
---
+++ src/pages/Registrate.js
@@ -0,0 +1,12 @@
+import Nav from "../components/nav/Nav";
+import Footer from "../components/footer/Footer";
+
+function Registrate(){
+    return(
+
+        <div>
+            <p>Hola mundo</p>
+        </div>
+    )
+}
+export default Registrate;
